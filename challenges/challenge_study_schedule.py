43b7a5cecf2e8c42d383803ea84205d8081e1291
pdragoni <paulohbfdf@gmail.com>
2023-01-25 16:55:28 -0300

    Requisito 1 - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,25 @@
+# print('permanent_period: ', permanence_period)
+# permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), ...]
+
+# print('target_time: ', target_time)
+# target_time = 5, expected = 3
+
+
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+
+    if not target_time:
+        return None
+    contador = 0
+    for a, b in permanence_period:
+        if not (isinstance(a, int) and isinstance(b, int)):
+            return None
+        if a > b:
+            return None
+        if a <= target_time <= b:
+            contador += 1
+    return contador
+
+
+#    if not (isinstance(x, int) and isinstance(y, int)):
+#       raise ValueError("Both x and y must be integers.")
