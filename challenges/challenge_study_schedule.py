05cdd591ed7a3aa333ea3e7c7844b4d1da1341c7
pdragoni <paulohbfdf@gmail.com>
2023-01-25 17:32:48 -0300

    [fix] complexidade infernal

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,14 +7,12 @@
 
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-
-    if type(target_time) != int:
+    if not target_time:
         return None
     contador = 0
     for a, b in permanence_period:
-        try:
-            if a <= target_time <= b:
-                contador += 1
-        except TypeError:
+        if (type(a) is not int) or (type(b) is not int):
             return None
+        if a <= target_time <= b:
+            contador += 1
     return contador
