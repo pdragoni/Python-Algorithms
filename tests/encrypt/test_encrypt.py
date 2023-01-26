2e1a0626234b741d5c9c4ab2226878c71b4b849b
pdragoni <paulohbfdf@gmail.com>
2023-01-25 23:05:07 -0300

    Requisito 2: Testes para encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,26 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+message = "password"
+even = 4
+odd = 5
+negative = -2
+zero = 0
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(message, negative) == "drowssap"
+    assert encrypt_message(message, odd) == "wssap_dro"
+    assert encrypt_message(message, even) == "drow_ssap"
+    with pytest.raises(TypeError, match=r"tipo inválido para key"):
+        encrypt_message(message, "a")
+    with pytest.raises(TypeError, match=r"tipo inválido para message"):
+        encrypt_message(negative, even)
+
+
+# def f():
+#     return 3
+
+
+# def test_function():
+#     assert f() == 4
