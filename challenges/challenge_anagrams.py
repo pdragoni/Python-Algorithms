bebadf00b7eb65e0928b2bf66d64d1996cf14d85
pdragoni <paulohbfdf@gmail.com>
2023-01-26 13:35:26 -0300

    [feat] função para ordenar as strings

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,16 @@
+def sort_string(string):
+    list_string = list(string)
+    # transforma a string em uma lista de caracteres
+
+    for a in range(len(list_string)):
+        for b in range(a + 1, len(list_string)):
+            if list_string[a] > list_string[b]:
+                list_string[a], list_string[b] = list_string[b], list_string[a]
+    # ordena a lista de caracteres sem usar sort ou sorted
+    return "".join(list_string)
+    # retorna a lista de caracteres como uma string
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
     raise NotImplementedError
